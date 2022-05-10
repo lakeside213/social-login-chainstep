0a3afc48c6bac184e746511bfae7bee97f42d2f4
Lekan Koku <kokulekan23@gmail.com>
2022-05-10 13:57:44 +0200

    feat: implemented google passport strategy

---
+++ src/config/passport.config.ts
@@ -0,0 +1,28 @@
+import passport from 'passport';
+import 'dotenv/config';
+
+import { Strategy as GoogleStrategy } from 'passport-google-oauth2';
+
+passport.serializeUser((req, user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+//Google strategy
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://localhost:3000/google/callback',
+      passReqToCallback: true,
+    },
+    (request, accessToken, refreshToken, profile, done) => {
+      console.log(profile, request, accessToken, refreshToken);
+      done(null, profile);
+    },
+  ),
+);
