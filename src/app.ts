074ba57ba4a3d2f10a256170bde2f24c8afff8dc
Lekan Koku <kokulekan23@gmail.com>
2022-05-10 12:57:17 +0200

    feat: added app init file

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import express from 'express';
+import cookieSession from 'cookie-session';
+import passport from 'passport';
+import { Routes } from '@interfaces/routes.interface';
+
+class App {
+  public app: express.Application;
+  public port: string | number;
+  public env: string;
+
+  constructor(routes: Routes[]) {
+    this.app = express();
+    this.port = process.env.PORT || 3000;
+    this.env = process.env.NODE_ENV || 'development';
+
+    this.app.set('view engine', 'ejs');
+    this.initializeMiddlewares();
+    this.initializeRoutes(routes);
+  }
+
+  public listen() {
+    this.app.listen(this.port, () => {
+      console.log(`=================================`);
+      console.log(`======= ENV: ${this.env} =======`);
+      console.log(`🚀 App listening on the port ${this.port}`);
+      console.log(`=================================`);
+    });
+  }
+
+  public getServer() {
+    return this.app;
+  }
+
+  private initializeMiddlewares() {
+    this.app.use(
+      cookieSession({
+        name: 'tuto-session',
+        keys: ['key1', 'key2'],
+      }),
+    );
+    this.app.use(passport.initialize());
+    this.app.use(passport.session());
+  }
+
+  private initializeRoutes(routes: Routes[]) {
+    routes.forEach((route) => {
+      this.app.use('/', route.router);
+    });
+  }
+}
+
+export default App;
