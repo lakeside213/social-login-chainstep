49bf33c3c92cbee8126769d208f9fcfebef26d2b
Lekan Koku <kokulekan23@gmail.com>
2022-05-10 13:46:26 +0200

    feat: implemented index controller

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 class IndexController {
   public index = (req: Request, res: Response, next: NextFunction): void => {
     try {
+      console.log('Testing');
       res.render('pages/index');
     } catch (error) {
       next(error);
